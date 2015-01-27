dc916d92fe832d3d9333be52dd40537e1a3ac17b
Carlos <carlos.saraiva@msn.com>
2015-01-27 16:57:26 -0200

    Implementing enemy class

--- js/enemy.js
+++ js/enemy.js
@@ -1,15 +1,18 @@
 // Enemies our player must avoid
-var Enemy = function(speed) {
+var Enemy = function(grid, position) {
     //Calling Entity class
-    this.speed = speed;
-    Entity.call(this, new PVector(0,0), 'images/enemy-bug.png')
+    Entity.call(this, grid,'images/enemy-bug.png', position)
 }
 Enemy.prototype = Object.create(Entity.prototype);
 Enemy.prototype.constructor = Enemy;
+Enemy.prototype.spawn = function(){
+  
+}
+
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x;
-    this.y;
+    this.grid.position[this.entityPosition.x][this.entityPosition.y].x++ * 40;
+
 }
